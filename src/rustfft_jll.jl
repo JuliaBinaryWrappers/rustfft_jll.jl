7409390535152b17ce5a239e0e16c7c65bb07d6f
jlbuild <juliabuildbot@gmail.com>
2026-09-13 02:28:58 +0000

    rustfft_jll build 0.6.0+0

--- src/rustfft_jll.jl
+++ src/rustfft_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("rustfft")
-JLLWrappers.@generate_main_file("rustfft", UUID("54eccfce-c43a-54d5-bb22-f204d2e1c97b"))
+JLLWrappers.@generate_main_file("rustfft", Base.UUID("54eccfce-c43a-54d5-bb22-f204d2e1c97b"))
 end  # module rustfft_jll
